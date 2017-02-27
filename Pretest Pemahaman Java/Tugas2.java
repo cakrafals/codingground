2ba02cfe1de67f0ab2d68f69bdae845c889c7adc
cakrafals <cakrafals@gmail.com>
2017-02-27 09:01:31 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas2.java
@@ -0,0 +1,17 @@
+public class Tugas2{
+
+   public static void main(String[] args){
+      String[] mahasiswa;
+        mahasiswa = new String[3];
+            mahasiswa[0] = "Cakra"; // isi element array 0
+            mahasiswa[1] = "Aji"; // isi element array 1
+            mahasiswa[2] = "Wicaksono"; // isi element array 2
+      
+      for(int i = 0; i < mahasiswa.length; i++){
+         System.out.println("Nama " + " " +mahasiswa[i]);
+
+      }
+
+   }
+
+}
