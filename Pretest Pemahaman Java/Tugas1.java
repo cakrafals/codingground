7fbfd2546defd40a602c047e63eabd107b889b3d
cakrafals <cakrafals@gmail.com>
2017-02-27 08:44:04 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+       String nama ="Cakra Aji Wicaksono";
+       String alamat = "Jepara";
+       System.out.println("Nama    : "+nama + "\n" +"Alamat     : "+alamat);
+     }
+}
