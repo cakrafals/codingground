558cc9fd81da6673a7bc935b9be8062852cf5513
cakrafals <cakrafals@gmail.com>
2017-03-05 21:08:11 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/BiodataTgs8.java
@@ -0,0 +1,9 @@
+public class BiodataTgs8{
+	String nama = "Cakra Aji Wicaksono ";
+	String alamat = "Jepara";
+	int umur = 21;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
